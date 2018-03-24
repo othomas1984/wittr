66f0942c03ea95d46f69bb03f2be21fab4fa878b
Owen Thomas <owen@owenhthomas.com>
2018-03-24 12:54:23 -0700

    3-08: Service Worker Dev Tools 2

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,6 @@
 self.addEventListener('fetch', function(event) {
   console.log(event.request);  
   // Add a comment then refresh the page to get a new waiting worker. Test with "sw-waiting"
+  // Then close all server windows (any pages loaded with localhost:8888) 
+  // and reopen them to install the new service worker. Test with "sw-active"
 });
